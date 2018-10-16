ae806c69a0bae7c3ff5fb30961d0fb6e0af6d7ac
Naga33 <courtenay.mueller@gmail.com>
2018-10-16 15:43:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/courtenay/langapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.courtenay.langapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
